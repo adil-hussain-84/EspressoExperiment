3e60bc148dbbbc4e55d631bc43c60ba21adf72d5
Adil Hussain <>
2020-09-04 15:30:26 +0100

    Added the `fragment` and `fragment-testing` dependencies to demonstrate that test running fails with a message: "Instrumentation run failed due to Process crashed."

--- app/src/androidTest/java/com/tazkiyatech/espressoexperiment/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/tazkiyatech/espressoexperiment/MainActivityInstrumentedTest.kt
@@ -5,13 +5,10 @@ import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class MainActivityInstrumentedTest {
 
     private lateinit var activityScenario: ActivityScenario<MainActivity>
